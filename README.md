66e7b3f38740b093959548b56977d2d4d29bd283
Ashish Sahu <itsashishsahu.inc@gmail.com>
2023-08-09 19:19:21 +0530

    * Major Update - 1. Folder structure set 2. Basic layout set

---
+++ README.md
@@ -0,0 +1,1 @@
+![screencapture-localhost-3000-2023-06-17-18_27_01](https://github.com/itsashishsahu/next13-deepmtrx)
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import { Hero } from "@/sections"
+import Head from "next/head"
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>deepmtrx.com</title>
+      </Head>
+      <Hero />
+    </>
+  )
+}
